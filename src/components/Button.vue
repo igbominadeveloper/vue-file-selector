f1123beb8e1e965247cbf5e7b9795707eab1e1b1
Favour Afolayan <favourafolayan@gmail.com>
2021-11-02 01:52:27 +0100

    :art: create landing page with button to trigger folders

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button">
+    <slot>Select Files</slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background: var(--color-blue);
+  color: var(--color-white);
+  border-radius: 4px;
+
+  padding: 5px 16px;
+  font-size: var(--font-size-md);
+  border: none;
+
+  cursor: pointer;
+}
+</style>
